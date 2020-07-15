0b300bd6542765f52a778c6c4dfeca0742a1ccd2
Dmitry Rykun <drykun@readdle.com>
2020-07-15 17:49:46 +0300

    Drawing using UIBezierPath instead of rects

--- Scalable Grid View/GridScrollView.swift
+++ Scalable Grid View/GridScrollView.swift
@@ -8,40 +8,62 @@
 
 import UIKit
 
+extension UIBezierPath {
+
+    convenience init(lineFrom p0: CGPoint, to p1: CGPoint) {
+        self.init()
+        move(to: p0)
+        addLine(to: p1)
+    }
+
+    convenience init(union paths: [UIBezierPath]) {
+        self.init()
+        paths.forEach(append)
+    }
+}
+
+extension Array where Element == UIBezierPath {
+    func joined() -> UIBezierPath {
+        UIBezierPath(union: self)
+    }
+}
+
+func rects(_ step: CGFloat, _ bounds: CGRect) -> UIBezierPath {
+
+    let verticals = (0...Int(bounds.width / step))
+        .map { CGPoint(x: (CGFloat($0) + floor(bounds.minX / step) + 1) * step, y: bounds.minY) }
+        .map { ($0, CGPoint(x: $0.x, y: $0.y + bounds.height))}
+
+    let horizontals = (0...Int(bounds.height / step))
+        .map { CGPoint(x: bounds.minX, y: (CGFloat($0) + floor(bounds.minY / step) + 1) * step) }
+        .map { ($0, CGPoint(x: $0.x + bounds.width, y: $0.y)) }
+
+    return (verticals + horizontals)
+            .map(UIBezierPath.init(lineFrom:to:))
+            .joined()
+}
+
 class GridScrollView: UIScrollView {
     
-    @IBInspectable var gridColor: UIColor = .white
-    
-    private let gridStep: CGFloat = 4
-    private let period: CGFloat = 10
-    private let minOpacity: CGFloat = 0.05
+    var gridColor: UIColor = .white
+    var lineWidth: CGFloat = 1
+    var gridStep: CGFloat = 4
+    var period: CGFloat = 10
+    var minOpacity: CGFloat = 0.05
 
     override var bounds: CGRect {
         didSet { setNeedsDisplay() }
     }
 
     private func drawGrid(in context: CGContext, zoomScaleDelta: CGFloat) {
-
         let gridScale = zoomScaleDelta * zoomScale / pow(period, floor(log(zoomScale) / log(period)))
-
         let alpha = sin(.pi * (gridScale / pow(period, 2)))
-
         guard alpha > minOpacity else { return }
-
-        gridColor.withAlphaComponent(alpha).setFill()
-
-        let k = gridStep * gridScale
-
-        let bx = (floor(bounds.minX / k) + 1) * k
-        let by = (floor(bounds.minY / k) + 1) * k
-
-        (0...Int(bounds.width / k))
-            .map { CGRect(x: CGFloat($0) * k + bx, y: bounds.minY, width: 1, height: bounds.height) }
-            .forEach(context.fill)
-
-        (0...Int(bounds.height / k))
-            .map { CGRect(x: bounds.minX, y: CGFloat($0) * k + by, width: bounds.width, height: 1) }
-            .forEach(context.fill)
+        gridColor.withAlphaComponent(alpha).setStroke()
+        let path = rects(gridScale * gridStep, bounds)
+        path.lineWidth = lineWidth
+        context.addPath(path.cgPath)
+        context.strokePath()
     }
 
     override func draw(_ rect: CGRect) {
